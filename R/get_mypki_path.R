3870fbddb5809747184bf36c36bbaff4dc4d114e
Joshua Bradley <jgbrad1@umd.edu>
2017-04-06 11:11:15 -0400

    Update get_mypki_path
    Add in check for windows and linux based OSes.

--- R/get_mypki_path.R
+++ R/get_mypki_path.R
@@ -1,6 +1,16 @@
-# default path to a .mypki file
+# retrieve the default path to a .mypki file
 get_mypki_path <- function() {
-  if (Sys.getenv('HOME') == '')
-    stop('Could not find HOME environment variable.')
-  path <- paste0(Sys.getenv('HOME'), .Platform$file.sep, '.mypki')
+  home_dir = ''
+  # Get home directory in Windows
+  if (.Platform$OS.type == 'windows') {
+    home_dir = Sys.getenv('USERPROFILE')
+    if (home_dir == '') warning('Could not find USERPROFILE environment variable.')
+  }
+  # Get home directory in Linux/Mac
+  if (.Platform$OS.type == 'unix') { # works for both linux and Mac OS
+    home_dir = Sys.getenv('HOME')
+    if (home_dir == '') warning('Could not find HOME environment variable.')
+  }
+  if (home_dir == '') stop()
+  path <- paste0(home_dir, .Platform$file.sep, '.mypki')
 }

79dfa64d878cba9f57aa12176bc39699db565bec
Joshua Bradley <jgbrad1@umd.edu>
2017-06-20 19:22:19 -0400

    Write encrypted key file

--- R/set_httr_config.R
+++ R/set_httr_config.R
@@ -16,7 +16,7 @@ set_httr_config <- function(ca_file = NULL, pki_file = NULL, pass = NULL) {
   cert_file <- tempfile()
   write_pem(x = p12$cert, path = cert_file)
   key_file <- tempfile()
-  write_pem(x = p12$key, path = key_file)
+  write_pem(x = p12$key, path = key_file, password = pass)
 
   # set httr config arguments globally so PKI authentication persists
   # for the whole R session.
@@ -26,5 +26,6 @@ set_httr_config <- function(ca_file = NULL, pki_file = NULL, pass = NULL) {
   #   sslkey: keyfile (.key but PEM formatted)
   httr::set_config(httr::config(cainfo = ca_file,
                                 sslcert = cert_file,
-                                sslkey = key_file))
+                                sslkey = key_file,
+                                keypasswd = pass))
 }

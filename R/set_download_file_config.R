72d82b5b7ede13f9ee0fdaddadd66f28840b6d86
Josh Bradley <jgbradley1@gmail.com>
2017-07-04 18:10:11 -0400

    write encrypted RSA pkcs#1 key file

--- R/set_download_file_config.R
+++ R/set_download_file_config.R
@@ -17,6 +17,17 @@ set_download_file_config <- function(ca_file = NULL, pki_file = NULL, pass = NUL
   write_pem(x = p12$cert, path = cert_file)
   key_file <- tempfile()
   write_pem(x = p12$key, path = key_file, password = pass)
+  key_file2 <- tempfile()
+  #system(paste0('openssl rsa -in ', key_file, ' -out ', key_file2, '-des -passin pass:', pass, ' -passout pass:', pass), ignore.stdout = TRUE, ignore.stderr = TRUE)
+  # write out encrypted RSA key file in PKCS#1 format
+  system2('openssl', args = c('rsa',
+                              '-in', key_file,
+                              '-out', key_file2,
+                              '-des',
+                              '-passin', paste0('pass:', pass),
+                              '-passout', paste0('pass:', pass)),
+          stdout = NULL,
+          stderr = NULL)
 
   # set download.file parameters globally so PKI authentication persists
   # for the entire R session.
@@ -29,7 +40,7 @@ set_download_file_config <- function(ca_file = NULL, pki_file = NULL, pass = NUL
   options(download.file.method = "curl")
   options(download.file.extra = paste("--cacert", ca_file,
                                       "--cert", cert_file,
-                                      "--key", key_file,
+                                      "--key", key_file2,
                                       "--pass", pass,
                                       "-L"))
 }
